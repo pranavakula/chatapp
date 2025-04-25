0ab0c948c0d124289a164b5f1f5c0c1d039a5ffb
lohithreddy25 <vempalli.lohithreddy@gmail.com>
2025-04-25 12:49:57 +0530

    same thing error to log in message controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -6,12 +6,12 @@ export const getUsersForSidebar = async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
         const filteredUsers = await User.find({
-            _id: {$ne: loggedInUserId},
+            _id: {$ne: loggedInUserId}
         
         }).select("-password");
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.error("Error fetching users for sidebar:", error);
+        console.log("Error fetching users for sidebar:", error.message);
         res.status(500).json({ message: "Internal server error" });
         
     }
@@ -31,7 +31,7 @@ export const getMessages = async (req, res) => {
         res.status(200).json(messages);
         
     } catch (error) {
-        console.error("Error fetching messages:", error);
+        console.log("Error fetching messages:", error.message);
         res.status(500).json({ message: "Internal server error" });
         
     }}
@@ -57,7 +57,7 @@ export const sendMessage = async (req, res) => {
         res.status(201).json(newMessage);
         
     } catch (error) {
-        console.error("Error sending message:", error);
+        console.log("Error sending message:", error.message);
         res.status(500).json({ message: "Internal server error" });
         
     }
