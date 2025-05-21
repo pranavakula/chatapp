4927b73e5f4ed8ccba70e39847ed371679b4d940
lohithreddy25 <vempalli.lohithreddy@gmail.com>
2025-05-21 10:08:39 +0530

    upto settings page

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -7,18 +7,18 @@ const AuthImagePattern = ({ title, subtitle }) => {
             {[...Array(9)].map((_, i) => (
               <div
                 key={i}
-                className={`aspect-square rounded-2xl ${
+                className={`aspect-square rounded-2xl bg-primary/10${
                   i % 2 === 0
-                    ? "bg-primary/50 animate-pulse"
-                    : "bg-primary/20"
+                    ? "animate-pulse"
+                    : ""
                 }`}
               />
             ))}
           </div>
   
           {/* Heading and Subtitle */}
-          <h2 className="text-2xl font-bold mb-2">{title}</h2>
-          <p className="text-gray-300">{subtitle}</p>
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-base-content/60">{subtitle}</p>
         </div>
       </div>
     );
